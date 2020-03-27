e18444e5d7918078ea4cad83580a1858f7f71336
dovy <dovydas.tamasauskas@gmail.com>
2020-03-27 17:17:39 +0000

    loader

--- src/screens/ClientList.js
+++ src/screens/ClientList.js
@@ -16,11 +16,10 @@ import {
   List,
   ListItem,
   Thumbnail,
-  Item,
-  Input,
-  StatusBar,
-  Form,
+  Spinner
 } from 'native-base';
+import { Circle } from 'better-react-spinkit';
+
 import CSHeader from '../components/CSHeader';
 
 import {getClientsList} from '../services/DatabaseServices';
@@ -30,12 +29,15 @@ export default class ClientList extends Component {
     super(props);
     this.state = {
       listItems: [],
+      isLoaded: false,
     };
   }
 
   async componentDidMount() {
     let data = await getClientsList();
     this.setState({listItems: data});
+
+    this.setState({isLoaded: true});
 
     console.log(this.state.listItems);
   }
@@ -44,46 +46,63 @@ export default class ClientList extends Component {
     const {error, isLoaded, listItems} = this.state;
 
     const pageTitle = 'Client List';
-    return (
-      <Container>
-        <CSHeader pageTitle={pageTitle} />
-        <Content>
-          <List>
-            {listItems.map(item => (
-              <ListItem thumbnail style={{marginTop: 20, marginBottom: 20}}>
-                <Left>
-                  <Thumbnail
-                    circular
-                    source={require('./assets/user.png')}
-                    style={{width: 50, height: 50}}
-                  />
-                </Left>
-                <Body>
-                  <Text style={{fontSize: 20}}>
-                    {item.name + ' ' + item.surname}
-                  </Text>
-                </Body>
-                <Right>
-                  <Button
-                    bordered
-                    style={{borderColor: '#103662', marginTop: -10, borderStartWidth: 1.5,
-                    borderEndWidth: 1.5,
-                    borderTopWidth: 1.5,
-                    borderBottomWidth: 1.5}}
-                    onPress={() =>
-                      this.props.navigation.push('ClientOverV', {
-                        client_id: item.client_id,
-                        nameSurname: item.name + ' ' + item.surname,
-                      })
-                    }>
-                    <Text style={{color: '#103662', fontSize: 20, fontWeight: '500'}}>View</Text>
-                  </Button>
-                </Right>
-              </ListItem>
-            ))}
-          </List>
-        </Content>
-      </Container>
-    );
+
+    if(!isLoaded) {
+      return (
+        <Container>
+          <CSHeader pageTitle={pageTitle} />
+          <Content contentContainerStyle={{
+            justifyContent: 'center',
+            flex: 1,
+            marginLeft: 10,
+            marginRight: 10,
+          }}>
+            <Spinner color='#103662'/>
+          </Content>
+        </Container>
+      )
+    } else {
+      return (
+        <Container>
+          <CSHeader pageTitle={pageTitle} />
+          <Content>
+            <List>
+              {listItems.map(item => (
+                <ListItem thumbnail style={{marginTop: 20, marginBottom: 20}}>
+                  <Left>
+                    <Thumbnail
+                      circular
+                      source={require('./assets/user.png')}
+                      style={{width: 50, height: 50}}
+                    />
+                  </Left>
+                  <Body>
+                    <Text style={{fontSize: 20}}>
+                      {item.name + ' ' + item.surname}
+                    </Text>
+                  </Body>
+                  <Right>
+                    <Button
+                      bordered
+                      style={{borderColor: '#103662', marginTop: -10, borderStartWidth: 1.5,
+                      borderEndWidth: 1.5,
+                      borderTopWidth: 1.5,
+                      borderBottomWidth: 1.5}}
+                      onPress={() =>
+                        this.props.navigation.push('ClientOverV', {
+                          client_id: item.client_id,
+                          nameSurname: item.name + ' ' + item.surname,
+                        })
+                      }>
+                      <Text style={{color: '#103662', fontSize: 20, fontWeight: '500'}}>View</Text>
+                    </Button>
+                  </Right>
+                </ListItem>
+              ))}
+            </List>
+          </Content>
+        </Container>
+      );
+    }
   }
 }
